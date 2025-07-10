bd112b56e8d0b507d104490ca62d66ee56fbdac3
bimvocado <163368218+bimvocado@users.noreply.github.com>
2025-07-11 02:21:08 +0900

    캐릭터 좌우이동, 중력 변환(중력 변환 시 기존 칸에 가는 것까지)

---
+++ GravityShift/Assets/Script/PlayerCtrl.cs
@@ -0,0 +1,128 @@
+using System.Numerics;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private Rigidbody rb;
+    public GameObject[] bottom;
+    public GameObject[] Right;
+    public GameObject[] Top;
+    public GameObject[] Left;
+    public int idx;
+    
+    public Vector3 gravity = new Vector3(0, -10, 0);
+    public int gravityStrength = 10;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        idx = 1; //시작 위치 가운데로
+
+    }
+
+    void Update()
+    {
+
+        // 캐릭터 중력 방향 전환
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (Input.GetKeyDown(KeyCode.A)) //왼쪽 벽으로 이동
+            {
+                Debug.Log("Shift+A 눌렸습니다");
+                if (gravity.y == -10) //중력 방향 Bottom -> Left
+                {
+                    rb.rotation = Quaternion.Euler(new Vector3(0, 0, -90));
+                    gravity = new Vector3(-10, 0, 0);
+                    transform.position = new Vector3(transform.position.x, Left[idx].transform.position.y, 0);
+                }
+                else if (gravity.x == -10) //중력 방향 Left -> Top
+                {
+                    rb.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+                    gravity = new Vector3(0, 10, 0);
+                    transform.position = new Vector3(Top[idx].transform.position.x, transform.position.y, 0);
+                }
+                else if (gravity.y == 10) //중력 방향 Top -> Right
+                {
+                    rb.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+                    gravity = new Vector3(10, 0, 0);
+                    transform.position = new Vector3(transform.position.x, Right[idx].transform.position.y, 0);
+                }
+                else if (gravity.x == 10) //중력 방향 Right -> Bottom
+                {
+                    rb.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                    gravity = new Vector3(0, -10, 0);
+                    transform.position = new Vector3(bottom[idx].transform.position.x, transform.position.y, 0);
+                }
+                
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                Debug.Log("Shift+D 눌렸습니다");
+                if (gravity.y == -10) //중력 방향 Bottom -> Right
+                {
+                    rb.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+                    gravity = new Vector3(10, 0, 0);
+                    transform.position = new Vector3(transform.position.x, Right[idx].transform.position.y, 0);
+                }       
+                else if (gravity.x == 10) //중력 방향 Right -> Top
+                {
+                    rb.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+                    gravity = new Vector3(0, 10, 0);
+                    transform.position = new Vector3(Top[idx].transform.position.x, transform.position.y, 0);
+                }
+                else if (gravity.y == 10) //중력 방향 Top -> Left
+                {
+                    rb.rotation = Quaternion.Euler(new Vector3(0, 0, -90));
+                    gravity = new Vector3(-10, 0, 0);
+                    transform.position = new Vector3(transform.position.x, Left[idx].transform.position.y, 0);
+                }
+                else if (gravity.x == -10) //중력 방향 Left -> Bottom
+                {
+                    rb.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                    gravity = new Vector3(0, -10, 0);
+                    transform.position = new Vector3(bottom[idx].transform.position.x, transform.position.y, 0);
+                }
+            }
+        }
+        //캐릭터 좌우 이동 구현 (shift 안눌렸을때)
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                Debug.Log("A만 눌림");
+                if (idx != 0)
+                {
+                    idx -= 1;
+                    if (gravity.y == -10) //중력 방향 Bottom
+                        transform.position = new Vector3(bottom[idx].transform.position.x, transform.position.y, 0);
+                    else if (gravity.x == -10) //중력 방향 Left
+                        transform.position = new Vector3(transform.position.x, Left[idx].transform.position.y, 0);
+                    else if (gravity.y == 10) //중력 방향 Top
+                        transform.position = new Vector3(Top[idx].transform.position.x, transform.position.y, 0);
+                    else if (gravity.x == 10) //중력 방향 Right
+                        transform.position = new Vector3(transform.position.x, Right[idx].transform.position.y, 0);
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                Debug.Log("D만 눌림");
+                if (idx != 2)
+                {
+                    idx += 1;
+                    if (gravity.y == -10) //중력 방향 Bottom
+                        transform.position = new Vector3(bottom[idx].transform.position.x, transform.position.y, 0);
+                    else if (gravity.x == -10) //중력 방향 Left
+                        transform.position = new Vector3(transform.position.x, Left[idx].transform.position.y, 0);
+                    else if (gravity.y == 10) //중력 방향 Top
+                        transform.position = new Vector3(Top[idx].transform.position.x, transform.position.y, 0);
+                    else if (gravity.x == 10) //중력 방향 Right
+                        transform.position = new Vector3(transform.position.x, Right[idx].transform.position.y, 0);
+                }
+            }  
+        }
+        rb.AddForce(gravity, ForceMode.Acceleration); //지정한 중력 방향으로 계속 힘 받기
+    }
+}
