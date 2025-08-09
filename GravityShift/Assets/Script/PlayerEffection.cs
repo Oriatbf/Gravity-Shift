b69dfb4969a3b266443d4024680106ed996738f9
Oriatbf <john31534249@gmail.com>
2025-08-09 22:05:21 +0900

    playerEff

--- GravityShift/Assets/Script/PlayerEffection.cs
+++ GravityShift/Assets/Script/PlayerEffection.cs
@@ -4,12 +4,41 @@ using UnityEngine;
 public class PlayerEffection : MonoBehaviour
 {
     [SerializeField] private Panel adArrowPanel, upArrowPanel;
+    private bool isShow = false;
+    [SerializeField] PlayerGravity playerGravity;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            adArrowPanel.SetPosition(PanelStates.Show,true);
+            isShow = !isShow;
+            if (isShow)
+            {
+                adArrowPanel.SetPosition(PanelStates.Show,true);
+                Vector3 rot = Vector3.zero;
+                switch (playerGravity)
+                {
+                    case PlayerGravity.Down:
+                        rot = new Vector3(0, 0, 0);
+                        break;
+                    case PlayerGravity.Up:
+                        rot = new Vector3(0, 0, 180);
+                        break;
+                    case PlayerGravity.Left:
+                        rot = new Vector3(0, 0, 270);
+                        break;
+                    case PlayerGravity.Right:
+                        rot = new Vector3(0, 0, 90);
+                        break;
+                }
+                adArrowPanel.transform.rotation = Quaternion.Euler(rot);
+            }
+            else
+            {
+                adArrowPanel.SetPosition(PanelStates.Hide,true);
+            }
+           
+            
         }
     }
 }
